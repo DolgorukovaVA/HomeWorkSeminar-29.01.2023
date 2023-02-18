b99dcec0c0c1daf8d0ac44637aedd7b59cf8a41a
Valentina <west.77@mail.ru>
2023-02-18 18:36:24 +0300

    Task 2 added

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,8 +1,51 @@
 ﻿// Показать двумерный массив размером m×n заполненный вещественными числами
-
-int[,] GetTable(int row, int col, int min, int max)
+int GetNumber(string message)
 {
-    int[,] table = new int[row, col];
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+double[,] InitMatrix(int m, int n)
+{
+    double[,] matrix = new double[m, n];
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.NextDouble() * 10;
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write("{0:0.0}|", matrix[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+int m = GetNumber("Введите число m");
+int n = GetNumber("Введите число n");
+double[,] matrix = InitMatrix(m, n);
+
+Console.WriteLine("Матрица:");
+
+PrintMatrix(matrix);
+
+
+/*double[,] GetTable(int row, int col, int min, int max)
+{
+    double[,] table = new double[row, col];
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < col; j++)
@@ -13,22 +56,23 @@ int[,] GetTable(int row, int col, int min, int max)
     return table;
 }
 
-void PrintTable(int[,] tab)
+void PrintTable(double[,] tab)
 {
     for (int i = 0; i < tab.GetLength(0); i++)
     {
         for (int j = 0; j < tab.GetLength(1); j++)
         {
-            System.Console.Write(tab[i, j] + " ");
+            Console.Write(tab[i, j] + " ");
         }
-        System.Console.WriteLine();
+        Console.WriteLine();
     }
 }
 
-int row = new Random().Next(5, 10);
-int col = new Random().Next(5, 10);
+int row = new Random().Next(5, 5);
+int col = new Random().Next(5, 5);
 int min = 1;
-int max = 100;
+int max = 10;
 
-int[,] matrix = GetTable(row, col, min, max);
+double[,] matrix = GetTable(row, col, min, max);
 PrintTable(matrix);
+*/
---
+++ Task 2/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+/*Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
+Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5
+*/
+
+//получает число с консоли
+int GetNumber(string message)
+{
+  Console.WriteLine(message);
+  int number = int.Parse(Console.ReadLine()??"");
+  return number;
+}
+
+int [,] InitMatrix(int m, int n)
+{
+  int[,] matrix = new int[m, n];
+  Random rnd = new Random();
+
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+     for (int j = 0; j < matrix.GetLength(1); j++)
+     {
+         matrix[i,j] = i + j;
+     }
+  }
+
+  return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+   for (int i = 0; i < matrix.GetLength(0); i++)
+   {
+      for (int j = 0; j < matrix.GetLength(1); j++)
+      {
+          Console.Write($"{matrix[i,j]} ");
+      }
+      Console.WriteLine();
+   }
+}
+
+int m = GetNumber("Введите число m");
+int n = GetNumber("Введите число n");
+int[,] matrix = InitMatrix(m, n);
+
+Console.WriteLine("Матрица:");
+PrintMatrix(matrix);
