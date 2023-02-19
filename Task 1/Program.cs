069429bf9f93325617dbe1ebd6e55e60109c1ff8
Valentina <west.77@mail.ru>
2023-02-19 11:47:53 +0300

    Task 1 2 3 4 changedd

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -29,7 +29,7 @@ void PrintMatrix(double[,] matrix)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            Console.Write("{0:0.0}|", matrix[i, j]);
+            Console.Write("{0:0.0}  ", matrix[i, j]);
         }
         Console.WriteLine();
     }
@@ -43,36 +43,3 @@ Console.WriteLine("Матрица:");
 PrintMatrix(matrix);
 
 
-/*double[,] GetTable(int row, int col, int min, int max)
-{
-    double[,] table = new double[row, col];
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < col; j++)
-        {
-            table[i, j] = new Random().Next(min, max + 1);
-        }
-    }
-    return table;
-}
-
-void PrintTable(double[,] tab)
-{
-    for (int i = 0; i < tab.GetLength(0); i++)
-    {
-        for (int j = 0; j < tab.GetLength(1); j++)
-        {
-            Console.Write(tab[i, j] + " ");
-        }
-        Console.WriteLine();
-    }
-}
-
-int row = new Random().Next(5, 5);
-int col = new Random().Next(5, 5);
-int min = 1;
-int max = 10;
-
-double[,] matrix = GetTable(row, col, min, max);
-PrintTable(matrix);
-*/
